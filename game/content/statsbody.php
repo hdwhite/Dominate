bcec180c26370ee4ac0e28b22575ca60399acffe
Harry White <hdwhite@vt.edu>
2014-05-28 21:19:40 +0000

    Added comments

--- game/content/statsbody.php
+++ game/content/statsbody.php
@@ -1,14 +1,19 @@
 <?php $numyears = floor(($gameinfo['numturns'] + 6)/ 5); ?>
 <div class=entry>
 	<h4>Statistics for <?=$gameinfo['name'] ?></h4>
-	<?php for($q = 0; $q < $numyears; $q += 10) { ?>
+	<?php
+	//Because of width constraints, SC ownership tables are only 10 years long
+	//and multiple tables will be used for longer games
+	for($q = 0; $q < $numyears; $q += 10) { ?>
 	<table border="1">
 		<thead>
 			<tr><th>Power</th>
 			<?php
+			//If the game is finished or not anonymous, show player names
 			if($gameinfo['press'] == 3 || $_gameinfo['status'] > 3)
 				echo("<th>Player</th>");
 			$curyear = $gameinfo['startyear'] + $numyears - 2;
+
 			for($i = $q + 1; $i < min($numyears, $q + 10); $i++)
 			{
 				$curyear = $gameinfo['startyear'] + $i - 1;
@@ -16,6 +21,9 @@
 					"$game/results/" . (($i - 1) * 5) ."'>$curyear</a></th>");
 			}
 			$curyear++;
+
+			//If this is the last year and the game has finished, make the final
+			//column labelled "Final"
 			if ($gameinfo['status'] > 3 && $q + 10 >= $numyears)
 			{
 				echo("<th><a href='http://hdwhite.org/dominate/game/" .
@@ -23,6 +31,8 @@
 				if($gameinfo['status'] == 5)
 					echo("<th>Pos</th><th>Points</th>");
 			}
+			//If Fall Retreats are done but not Winter Adjustments, link to the
+			//former instead
 			else
 			{
 				if($curyear > $curturn->getyear())
@@ -32,17 +42,26 @@
 					echo("<th><a href='http://hdwhite.org/dominate/game/" .
 						"$game/results/" . (($i - 1) * 5) . "'>$curyear</a></th>");
 			}
-		echo("</tr></thead>");
+		echo("</tr></thead><tbody>");
+
+		//Now for the table body
+		//Iterate over each power
 		foreach ($stattable as $curpower)
 		{
+			//Make the table colourful!
 			echo("<tr><td class=\"" . substr($curpower['name'], 0, 1) . "\">" .
 				$curpower['name'] . "</td>");
+
+			//Show player name if we should
 			if($gameinfo['press'] == 3 || $gameinfo['status'] > 3)
 				echo("<td>" . implode(" / ", $curpower['players']) . "</td>");
+
 			for ($j = $q; $j < min($numyears, $q + 10); $j++)
 			{
 				echo("<td>");
 				$sc = $curpower[$gameinfo['startyear'] + $j];
+				//Bold the entry if they were in the lead for that year
+				//Italicise it if they were eliminated
 				switch($sc)
 				{
 					case 0:
@@ -56,14 +75,22 @@
 				}
 				echo("</td>");
 			}
+
+			//If the game is over and was for stats, display their final rank
+			//and points tally
 			if ($gameinfo['status'] == 5 && $numyears <= $q + 10)
 				echo("<td>" . $curpower['rank'] . "</td>" .
 					"<td>" . $curpower['points'] . "</td>");
 			echo("</tr>");
 		}
-		echo("<tfoot><tr></tr><tr><td>Index</td>");
+		//Footer time
+		echo("</tbody><tfoot><tr></tr><tr><td>Index</td>");
 		if($gameinfo['press'] == 3 || $gameinfo['status'] > 3)
 			echo("<td>N/A</td>");
+
+		//The index is equal to the average of the sum of the squares of the
+		//total SC count for each player in a given year. The higher the index,
+		//the further along a game has progressed
 		for ($i = $q; $i < min($numyears, $q + 10); $i++)
 		{
 			$index = 0;
@@ -79,6 +106,7 @@
 	</tfoot></table>
 	<?php echo("<br>"); } ?>
 </div>
+
 <?php if($gameinfo['standard'] == 'y') { ?>
 <div class="entry">
 	<h4>SC Ownership by year</h4>
--- game/objects/Turn.php
+++ game/objects/Turn.php
@@ -1,13 +1,20 @@
 <?php
+
+//The Turn class
+//Each Turn represerts a year, a season, and a phase
 class Turn
 {
 	protected $startyear, $turnnum, $displaylink;
+	//The games database keeps track of the starting year and the number of
+	//elapsed turns, so that is what we will use as well
 	public function __construct($startyear, $turnnum)
 	{
 		$this->startyear = $startyear;
 		$this->turnnum = $turnnum;
 		$this->displaylink = true;
 	}
+
+	//When printing the Turn, do we want it to show up as a link?
 	public function showlink($newdisplay)
 	{
 		$this->displaylink = $newdisplay;
@@ -16,10 +23,15 @@ class Turn
 	{
 		return $this->turnnum;
 	}
+
+	//Five turns in a year
 	public function getyear()
 	{
 		return $this->startyear + floor($this->turnnum / 5);
 	}
+
+	//There's nothing inherently wrong with a negative turn number, just as long
+	//as it's not used as a link
 	public function getprevious()
 	{
 		return new Turn($this->startyear, $this->turnnum - 1);
@@ -28,12 +40,30 @@ class Turn
 	{
 		return new Turn($this->startyear, $this->turnnum + 1);
 	}
+
+	//Probably could just use display() instead of creating another function,
+	//but oh well
 	public function isretreat()
 	{
 		if($this->turnnum % 5 == 2 || $this->turnnum % 5 == 4)
 			return true;
 		return false;
 	}
+
+	//Prints Turn information in the given format
+	//Think of this like the date() function
+	//Options:
+	//  'y': Prints the year
+	//  's': Prints a one-letter abbreviation of the season
+	//  'S': Prints the full name of the season
+	//  'p': Prints a one-letter abbreviation of the phase
+	//  'P': Prints the full name of the phase
+	//  'F': Prints a description of the season
+	//  All else: Print as written
+	//
+	// For example, display('S y F') might return
+	//  "Spring 1901 Movement and Retreat",
+	// while display('ysp') would return "1901SM"
 	public function display($format)
 	{
 		$shortseasons = array('W', 'S', 'S', 'F', 'F');
@@ -71,6 +101,9 @@ class Turn
 		}
 		return $outstring;
 	}
+
+	//Given a game number, prints the Turn's year and season and links to it if
+	//displaylink is set to true
 	public function printurl($game)
 	{
 		$url = $this->display('ys');
@@ -78,11 +111,13 @@ class Turn
 			$url = "<a href='/dominate/game/$game/results/$this->turnnum'>$url</a>";
 		return $url;
 	}
+
+	//Returns true if compared turns refer to the same year, season, and phase
 	public function equals($turn)
 	{
 		if(!isset($turn))
 			return false;
-		if($turn->getturn() == $this->turnnum)
+		if($turn->display('ysp') == $this->display('ysp'))
 			return true;
 		return false;
 	}
